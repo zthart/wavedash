425fe88c9ad75f9fece0ed2eb851bf07ef97365a
Zachary Hart <zthart96@gmail.com>
2019-10-05 21:48:25 -0400

    Basic face iteration

--- modules/face.lua
+++ modules/face.lua
@@ -2,6 +2,49 @@
 -- @module face
 
 local face = {}
+local face_mt = {}
 
-return face
+local function new(v1, v2, v3)
+	return setmetatable({
+		v1=v1,
+		v2=v2,
+		v3=v3
+	}, face_mt)
+end
+
+--- Public constructor
+-- @param table v1 Vertex, texcoord (optional), and normal (optional) indices of the form {v1, vt1, vn1}
+-- @param table v2 Vertex, texcoord (optional), and normal (optional) indices of the form {v2, vt2, vn2}
+-- @param table v3 Vertex, texcoord (optional), and normal (optional) indices of the form {v3, vt3, vn3}
+function face.new(v1, v2, v3)
+	assert(type(v1) == "table", "new: Argument v1 must be a table of the form {v1, [vt1], [vn1]}")
+	assert(type(v2) == "table", "new: Argument v2 must be a table of the form {v2, [vt2], [vn2]}")
+	assert(type(v3) == "table", "new: Argument v3 must be a table of the form {v3, [vt3], [vn3]}")
+	
+	assert(v1[1], "new: v1 must contain a vertex index")
+	assert(v2[1], "new: v2 must contain a vertex index")
+	assert(v3[1], "new: v3 must contain a vertex index")
+
+	local idx_types = {"vertex, texture, normal"}
+	local vsets = {v1, v2, v3}
+
+	for i=1, #vsets do
+		for j=1, #vsets[i] do
+			if vsets[i][j] ~= nil then
+				assert(type(vsets[i][j]) == "number", "new: All index values must be of type <number>")
+			end
+		end
+	end
+
+	return new(v1, v2, v3)
+end
+
+
+face_mt.__index = face
+
+function face_mt.__call(_, v1, v2, v3)
+	return face.new(v1, v2, v3)
+end
+
+return setmetatable({}, face_mt)
 
--- modules/object.lua
+++ modules/object.lua
@@ -3,18 +3,24 @@
 
 local modules = (...):gsub('%.[^%.]+$', '') .. "."
 local vertex = require(modules .. "vertex")
+local normal = require(modules .. "normal")
+local texcoord = require(modules .. "texcoord")
 local face = require(modules .. "face")
 local open = io.open
 local lines = io.lines
 local object = {}
 local object_mt = {}
 
-local function new(vertices, normals, texcoords, faces)
+--- Private constructor
+local function new(vertices, normals, texcoords, faces, unsupported)
 	assert(type(vertices) == "table", "new: Object must be created with vertices")
-	return setmetatable({vertices = vertices, 
-						 normals = normals or {},
-						 texcoords = texcoords or {},
-						 faces = faces or {}}, object_mt)
+	return setmetatable({
+		vertices = vertices, 
+		normals = normals,
+		texcoords = texcoords,
+		faceset = faces,
+		unsupported = unsupported
+	}, object_mt)
 end
 
 --- Public constructor
@@ -25,17 +31,18 @@ function object.parse(filepath)
 	local file = assert(open(filepath))
 
 
-	local datasplit = function(toSplit)
+	local datasplit = function(toSplit, on)
+		assert(on ~= "", "datasplit: Cannot split on an empty string")
 		local out = {}
 
 		local outField, start = 1, 1
-		local first, last = toSplit:find(" ", start)
+		local first, last = toSplit:find(on, start)
 
 		while first do
 			out[outField] = toSplit:sub(start, first-1)
 			outField = outField + 1
 			start = last + 1
-			first, last = toSplit:find(" ", start)
+			first, last = toSplit:find(on, start)
 		end
 
 		out[outField] = toSplit:sub(start)
@@ -45,19 +52,64 @@ function object.parse(filepath)
 
 	-- Create local tables for each of our supported datapoints
 	local verts, texcoords, norms, faces = {}, {}, {}, {}
+	local found_unsupported = false
 
 	for line in lines(filepath) do
 		if line:len() > 0 and line:sub(1, 1) ~= "#" then
-			local data = datasplit(line)
+			local data = datasplit(line, " ")
 
 			if data[1] == "v" then
 				local vert = vertex.new(tonumber(data[2]), tonumber(data[3]), tonumber(data[4]), tonumber(data[5]))
 				verts[#verts+1] = vert
+			elseif data[1] == "vn" then
+				local norm = normal.new(tonumber(data[2]), tonumber(data[3]), tonumber(data[4]))
+				norms[#norms+1] = norm
+			elseif data[1] == "vt" then
+				local tex = texcoord.new(tonumber(data[2]), tonumber(data[3]), tonumber(data[4]))
+				texcoords[#texcoords+1] = tex
+			elseif data[1] == "f" then
+				local vsets = {}
+				for i=2, #data do
+					local sface = datasplit(data[i], "/")
+					vsets[i-1] = {tonumber(sface[1]), tonumber(sface[2]), tonumber(sface[3])}
+				end
+				faces[#faces+1] = face.new(vsets[1], vsets[2], vsets[3])
+			else
+				found_unsupported = true
 			end
 		end
 	end
 
-	return new(verts, norms, texcoords, faces)
+	return new(verts, norms, texcoords, faces, found_unsupported)
+end
+
+--- Iterator for faces
+-- @tparam object obj Object over which to iterate
+-- @treturn verts Component vertices
+-- @treturn texcoords Component texcoords
+-- @treturn norms Component normals
+function object.faces(obj)
+	local face = 1
+	return function()
+		while face <= #obj.faceset do
+			local verts = {}
+			local texcoords = {}
+			local norms = {}
+
+			for _, vset in ipairs(obj.faceset[face]) do
+				verts[#verts+1] = obj.vertices[vset[1]]
+				if vset[2] ~= nil then
+					texcoords[#texcoords+1] = obj.texcoords[vset[2]]
+				end
+				if vset[3] ~= nil then
+					norms[#norms+1] = obj.normals[vset[3]]
+				end
+			end
+			face = face + 1
+			return verts, texcoords, norms
+		end
+		return nil
+	end
 end
 
 object_mt.__index = object
